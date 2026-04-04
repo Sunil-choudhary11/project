62aa6e14bc26602d25e48723ae03e6bc77bac39e
Sunil-choudhary11 <sunilcg1313@gmail.com>
2026-04-04 12:09:05 +0530

    Remove empty line in index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <title>Student Management System</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <style>
 html, body {
     height: 100%;
