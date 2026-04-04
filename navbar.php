6d717c7876154c0ad35f13b46779b814719c3310
Sunil-choudhary11 <sunilcg1313@gmail.com>
2026-04-04 12:08:28 +0530

    Refactor navbar.php to improve session handling

--- navbar.php
+++ navbar.php
@@ -5,7 +5,6 @@ if(!isset($_SESSION['admin'])){
 }
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
 <a class="navbar-brand" href="dashboard.php">Student Management</a>
